3f4bef42e6454d09827aded147f9b4f792355fa4
SimonSongGit <simonsong004@gmail.com>
2020-03-04 14:15:01 -0500

    modified

--- 1142_User Activity for the Past 30 Days II.sql
+++ 1142_User Activity for the Past 30 Days II.sql
@@ -2,6 +2,6 @@ select round(ifnull(count(distinct session_id)/count(distinct user_id),0),2) as 
 from
 (
 select distinct user_id,session_id
-from activity_1142
+from activity
 where datediff('2019-07-27',activity_date)<=30
 ) t
