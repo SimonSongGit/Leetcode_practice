58765f4aee754df1d2b37d9f62875ae832baf608
SimonSongGit <simonsong004@gmail.com>
2020-04-02 09:22:23 -0400

    added

--- 1251_Average Selling Price.sql
+++ 1251_Average Selling Price.sql
@@ -3,4 +3,11 @@ from prices p
 	          inner join unitssold u 
               on p.product_id=u.product_id 
 				 and (purchase_date between start_date and end_date)
+group by p.product_id;
+
+
+
+select p.product_id as ids,round(sum(price*units)/sum(units),2) as average_price
+from prices p,unitssold u
+where p.product_id = u.product_id and (u.purchase_date between p.start_date and p.end_date)
 group by p.product_id
---
+++ 1378_Replace Employee ID With The Unique Identifier.sql
@@ -0,0 +1,3 @@
+select unique_id,name
+from employees e1
+     left join employeeuni e2 on e1.id = e2.id
