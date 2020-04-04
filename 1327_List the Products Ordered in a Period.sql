06b25a3e1a4fd6d43ff66403052f0c5a336560e5
SimonSongGit <simonsong004@gmail.com>
2020-04-03 20:20:20 -0400

    modified

--- 1327_List the Products Ordered in a Period.sql
+++ 1327_List the Products Ordered in a Period.sql
@@ -1,5 +1,5 @@
 select product_name, sum(unit) as unit 
 from orders o,products p
-where Month(order_date) = 2 and o.product_id=p.product_id
+where left(order_date,7) = '2020-02' and o.product_id=p.product_id
 group by o.product_id
-having unit>=100
+having sum(unit)>=100
