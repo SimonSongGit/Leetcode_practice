08cab0669709dbce5504d2aa04efe801b1d46082
SimonSongGit <simonsong004@gmail.com>
2020-04-08 11:42:31 -0400

    modified

--- 1084_Sales Analysis III.sql
+++ 1084_Sales Analysis III.sql
@@ -2,5 +2,12 @@ select product_id,product_name
 from product
 where product_id not in (select product_id
                          from sales
-                         where sale_date not between '2019-01-01' AND '2019-03-31')
+                         where sale_date not between '2019-01-01' AND '2019-03-31');
+                         
+                         
+select p.product_id, product_name
+from product p,sales s
+where p.product_id = s.product_id
+group by s.product_id
+having max(sale_date) between '2019-01-01' and '2019-03-31'
                         
--- 1322_Ads Performance.sql
+++ 1322_Ads Performance.sql
@@ -4,4 +4,13 @@ select ad_id, round((case when sum(action='Clicked' or action='Viewed')=0 then 0
  
  from ads
  group by ad_id
- order by ctr desc,ad_id asc
+ order by ctr desc,ad_id asc;
+ 
+ 
+select ad_id, round(ifnull(100*sum(action_='Clicked')/(sum(action_='Viewed')+sum(action_='Clicked')),0),2) as ctr
+from ads
+group by ad_id
+order by ctr desc, ad_id asc;
+ 
+ 
+ 
