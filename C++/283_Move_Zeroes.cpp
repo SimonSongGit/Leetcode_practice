dfd776ae65f8d19880112a778285c9ec9a4a525a
SimonSongGit <simonsong004@gmail.com>
2020-04-16 10:16:08 -0400

    added

---
+++ C++/283_Move_Zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int tmp = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[tmp++] = nums[i];
+            }
+        }
+
+        for (int j=tmp;j<nums.size();j++)
+        {
+            nums[j] = 0;
+        }
+
+    }
+};
