5a576d341badd0e5a7eb3ed0c784dfea11e5d70f
SimonSongGit <simonsong004@gmail.com>
2020-04-18 14:23:33 -0400

    modified

--- MySQL/1045_Customers Who Bought All Products.sql
+++ MySQL/1045_Customers Who Bought All Products.sql
@@ -3,4 +3,11 @@ from customer
 group by customer_id
 having count(distinct product_key)=(select count(*)
                                     from product
-								   )
+								   );
+                                   
+                                   
+select customer_id
+from customer c,product p
+where c.product_key=p.product_key
+group by customer_id
+having count(distinct p.product_key)=(select count(*) from product)
