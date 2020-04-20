4cfacc3d2c03fcf6aee1cd2bc954899ec7799f77
SimonSongGit <simonsong004@gmail.com>
2020-04-20 13:29:40 -0400

    modified

--- Python/104_Maximum_Depth_of_Binary_Tree.py
+++ Python/104_Maximum_Depth_of_Binary_Tree.py
@@ -5,6 +5,28 @@
 #         self.left = None
 #         self.right = None
 
+#recursive
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right)) if root else 0
+
+
+
+#iterative
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        depth = 0
+        level = [root] if root else []
+        
+        while level:
+            depth += 1
+            tmp = []
+            
+            for val in level:
+                if val.left:
+                    tmp.append(val.left)
+                if val.right:
+                    tmp.append(val.right)
+            
+            level = tmp
+        return depth
--- Python/617_Merge_Two_Binary_Trees.py
+++ Python/617_Merge_Two_Binary_Trees.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+#recursive method
 class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
         if t1 and t2:
@@ -15,3 +16,32 @@ class Solution:
         
         else:
             return t1 or t2
+
+
+#iterative method
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 is None:
+            return t2
+        
+        stack = []
+        stack.append((t1,t2))
+        
+        while stack:
+            node = stack.pop()
+            if node[1] is None:
+                continue
+            node[0].val += node[1].val
+            
+            if node[0].left is None:
+                node[0].left = node[1].left
+            else:
+                stack.append((node[0].left,node[1].left))
+                
+                
+            if node[0].right is None:
+                node[0].right = node[1].right
+            else:
+                stack.append((node[0].right,node[1].right))
+        return t1
+
