5e3a79a2673c10b4354e87ef0082a39256b7e409
SimonSongGit <simonsong004@gmail.com>
2020-04-20 09:39:43 -0400

    modified

--- MySQL/1164_Product Price at a Given Date.sql
+++ MySQL/1164_Product Price at a Given Date.sql
@@ -13,4 +13,14 @@ from
 										 where change_date<='2019-08-16' 
                                          group by product_id
                                         )
-	  ) t2 on t1.product_id=t2.product_id
+	  ) t2 on t1.product_id=t2.product_id;
+      
+      
+      
+select p.product_id,if(isnull(pid),10,new_price) as price
+from products p
+     left join (select product_id as pid,max(change_date) as dd
+				from products 
+				where datediff('2019-08-16',change_date)>=0
+				group by pid) t2 on p.product_id=t2.pid 
+where t2.pid is null or p.change_date=t2.dd
--- MySQL/608_Tree Node.sql
+++ MySQL/608_Tree Node.sql
@@ -12,7 +12,11 @@ order by b.id;
 select a.id,
        if(isnull(a.p_id),'Root',if(a.id in (select p_id from tree),'Inner','Leaf')) as Type
 from tree a
-order by a.id
+order by a.id;
+
+select distinct t1.id,if(isnull(t1.p_id),'root',if(t2.p_id,'inner','leaf')) as 'Type'
+from tree t1
+     left join tree t2 on t1.id=t2.p_id
 
 
 
