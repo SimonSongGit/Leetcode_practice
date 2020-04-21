24688bf708755be3118797cdd0ec4217a61f05f1
SimonSongGit <simonsong004@gmail.com>
2020-04-21 09:30:05 -0400

    modified

--- MySQL/1321_Restaurant Growth.sql
+++ MySQL/1321_Restaurant Growth.sql
@@ -1,3 +1,13 @@
+select t1.visited_on,sum(amount) as amount,round(sum(amount)/7,2) as average_amount
+from (select c1.visited_on
+      from customer c1
+           inner join customer c2 on datediff(c1.visited_on,c2.visited_on)=6
+      group by c1.visited_on) t1
+              left join customer t2 on datediff(t1.visited_on,t2.visited_on)<=6 and datediff(t1.visited_on,t2.visited_on)>=0
+group by t1.visited_on
+order by t1.visited_on;
+
+
 select a.visited_on, sum(daily_total) as amount, round(avg(daily_total),2) as average_amount
 from
 	  (
