0f3f5323b722916dd7434edf27521678061c6d49
SimonSongGit <simonsong004@gmail.com>
2020-04-23 09:04:44 -0400

    modified

--- MySQL/1212_Team Scores in Football Tournament.sql
+++ MySQL/1212_Team Scores in Football Tournament.sql
@@ -13,4 +13,16 @@ from teams t
 								end) as num_points
 	  from matches) t1 on t.team_id=t1.team
 group by team_id
-order by num_points desc, team_id asc
+order by num_points desc, team_id asc;
+
+
+
+select team_id,team_name,sum(ifnull(goal,0)) as num_points
+from teams t
+	left join (select host_team as team,if(host_goals>guest_goals,3,if(host_goals<guest_goals,0,1)) as goal
+               from matches
+               union all
+               select guest_team as team,if(host_goals<guest_goals,3,if(host_goals>guest_goals,0,1)) as goal
+               from matches) t1 on t.team_id=t1.team
+group by t.team_id
+order by num_points desc,team_id asc
