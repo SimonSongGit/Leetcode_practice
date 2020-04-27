31e93f66be683bbc66b3dcceafa8502ad504383b
SimonSongGit <simonsong004@gmail.com>
2020-04-27 11:50:41 -0400

    modified

--- MySQL/580_Count Student Number in Departments.sql
+++ MySQL/580_Count Student Number in Departments.sql
@@ -1,4 +1,4 @@
-select dept_name, count(ifnull(student_id,null)) as student_number
+select dept_name, count(student_id) as student_number
 from department d
      left join student s on d.dept_id=s.dept_id
 group by dept_name
