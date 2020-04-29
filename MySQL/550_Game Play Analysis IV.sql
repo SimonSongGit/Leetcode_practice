fe124a3db8564039e48cbf223c96a386dc9d67cc
SimonSongGit <simonsong004@gmail.com>
2020-04-29 08:08:21 -0400

    modified

--- MySQL/550_Game Play Analysis IV.sql
+++ MySQL/550_Game Play Analysis IV.sql
@@ -4,4 +4,9 @@ from activity t1,
 				from activity a
 				where (a.player_id,to_days(a.event_date)+1) in (select player_id,to_days(event_date)
 																from activity
-																)) t2
+																)) t2;
+                                                                
+                                                                													
+select count(t2.id)/(select count(distinct player_id) from activity)
+from   (select a1.player_id as id from activity a1
+        inner join activity a2 on a1.player_id=a2.player_id and to_days(a1.event_date)+1 = to_days(a2.event_date)) t2 
