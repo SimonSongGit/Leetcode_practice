0e981301660dd14a486077a3085d3290683b5ec9
SimonSongGit <simonsong004@gmail.com>
2020-05-04 08:39:02 -0400

    modified

--- MySQL/614_Second Degree Follower.sql
+++ MySQL/614_Second Degree Follower.sql
@@ -2,4 +2,11 @@ select a.follower,count(distinct b.follower) as num
 from follow a
      inner join follow b on a.follower=b.followee
 group by a.follower
-order by a.follower
+order by a.follower;
+
+
+select f1.followee,count(distinct f1.follower) as num
+from follow f1,follow f2
+where f1.followee=f2.follower
+group by f1.followee
+order by f1.followee
