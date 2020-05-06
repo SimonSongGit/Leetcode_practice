71bcc5f14b7fe360125b6cf2187bb71726ebe7b7
SimonSongGit <simonsong004@gmail.com>
2020-05-06 09:51:45 -0400

    modified

--- MySQL/1225_Report Contiguous Dates.sql
+++ MySQL/1225_Report Contiguous Dates.sql
@@ -1,3 +1,22 @@
+#Rank the succeeded and failed dates then select min and max in each ranked group
+select st as period_sate,min(d) as start_date,max(d) as end_date
+from 
+(
+select d,@r:= (case when @pre=state then @r else @r+1 end) as r,@pre:=state as st
+from (
+select 'succeeded' as state, success_date as d
+from succeeded s1
+union all
+select 'failed' as state, fail_date as d
+from failed f1) t1, (select @r:=0,@pre:='Unkown') t2
+where d between '2019-01-01' and '2019-12-31'
+order by d) t3
+group by r
+order by start_date;
+
+
+
+
 (select period_date,start_date,min(end_date) as end_date 
 from
     (select 'succeeded' as period_date,a.success_date as start_date
