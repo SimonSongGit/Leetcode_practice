b9e0fd78f04d410081915bb7116d19cc0bc2cad9
SimonSongGit <simonsong004@gmail.com>
2020-05-26 13:02:44 -0400

    modified

--- MySQL/615_Average Salary Departments VS Company.sql
+++ MySQL/615_Average Salary Departments VS Company.sql
@@ -4,4 +4,13 @@ from (select left(pay_date,7) as pay_date,department_id,avg(amount) as davg
 			inner join employee e on s.employee_id=e.employee_id
 	 group by pay_date,department_id) t1,
      (select left(pay_date,7) as pay_date,avg(amount) cavg from salary group by pay_date) t2
-where t1.pay_date=t2.pay_date
+where t1.pay_date=t2.pay_date;
+
+
+select pay_month, depart as department_id,if(avg(amount)>average,'higher',if(avg(amount)=average,'same','lower')) as comparison
+from (select left(pay_date,7) as pay_month, e.department_id as depart,s.employee_id,
+			 avg(amount) over (partition by left(pay_date,7)) as average, amount
+      from salary s
+            inner join employee e on s.employee_id=e.employee_id) t1
+group by pay_month,depart
+
