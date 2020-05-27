d4e18054ff5a5e5c6185b977129cd716cce13d0a
SimonSongGit <simonsong004@gmail.com>
2020-05-27 11:14:16 -0400

    modified

--- MySQL/571_Find Median Given Frequency of Numbers.sql
+++ MySQL/571_Find Median Given Frequency of Numbers.sql
@@ -1,11 +1,17 @@
 select avg(Number) as median
-from (
-select Number,cumsum as end_range,if(cumsum=(select min(Frequency) from numbers),0,@pre) as start_range,@pre:=cumsum
-from (
-select Number,(sum(Frequency) over (order by Number)) as cumsum
-from numbers) t1, (select @pre:=0) t2 
-) t3
+from (select Number,cumsum as end_range,if(cumsum=(select min(Frequency) from numbers),0,@pre) as start_range,@pre:=cumsum
+      from (select Number,(sum(Frequency) over (order by Number)) as cumsum from numbers) t1, (select @pre:=0) t2) t3
 where (floor(((select sum(Frequency) from numbers)+1)/2) > start_range and
       floor(((select sum(Frequency) from numbers)+1)/2) <= end_range) or
       (floor((select sum(Frequency) from numbers)/2)+1 > start_range and
-      floor((select sum(Frequency) from numbers)/2)+1 <= end_range)
+      floor((select sum(Frequency) from numbers)/2)+1 <= end_range);
+      
+      
+
+
+select avg(Number) as median
+from (select Number, cumsum as end_range, if(cumsum=(select min(Frequency) from numbers),0,@pre) as start_range,@pre:=cumsum
+      from (select Number,(sum(Frequency) over (order by Number)) as cumsum
+            from numbers) t1, (select @pre:=0, @total:=(select sum(Frequency) from numbers)) t2) t3
+where (floor((@total+1)/2)>start_range and  floor((@total+1)/2)<=end_range)  or
+      (floor((@total/2+1))>start_range and floor((@total/2+1))<=end_range)
