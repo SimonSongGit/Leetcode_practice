020a00b9177bc7e6836f732961d1bc6f09819499
SimonSongGit <simonsong004@gmail.com>
2020-05-28 17:42:15 -0400

    modified

--- MySQL/1336_Number of Transactions per Visit.sql
+++ MySQL/1336_Number of Transactions per Visit.sql
@@ -21,7 +21,22 @@ from
 						  group by t.user_id,t.transaction_date) t1
                           group by trans_count
                     ) t3 
-		 on t2.transactions_count=t3.t_count
+		 on t2.transactions_count=t3.t_count;
+         
+         
+         
+#similar method but more concise
+select t_count as transactions_count, ifnull(visits_count,0) as visits_count
+from (select @count:=@count+1 as t_count
+      from (select @count:=-1, @max_count:=ifnull(max(c),0)
+            from (select count(*) as c from transactions group by user_id,transaction_date) tmp1) tmp2
+	       cross join (select user_id from transactions) tmp3
+     where @count<@max_count) tmp4
+left join (select trans_count as transactions_count,count(trans_count) as visits_count
+           from (select v.user_id, t1.user_id as ids, visit_date, transaction_date, count(t1.user_id) as trans_count
+                 from visits v
+	             left join transactions t1  on v.user_id=t1.user_id and v.visit_date=t1.transaction_date
+	       group by transaction_date, v.user_id) t2 group by trans_count) t3 on tmp4.t_count=t3.transactions_count
 
 
 
