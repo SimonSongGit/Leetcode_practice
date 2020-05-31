f9fea51e40cc8590d282f992c54a425c5239852b
SimonSongGit <simonsong004@gmail.com>
2020-05-31 12:19:38 -0400

    modified

--- MySQL/1407_Top Travellers.sql
+++ MySQL/1407_Top Travellers.sql
@@ -2,5 +2,11 @@ select name,ifnull(sum(distance),0) as travelled_distance
 from users u
 	 left join rides r on u.id=r.user_id
 group by u.id
-order by travelled_distance desc, name asc
+order by travelled_distance desc, name asc;
+
+
+select distinct name, ifnull((sum(distance) over (partition by user_id)),0) as travel_distance
+from rides r
+     right join users u on r.user_id = u.id 
+order by travel_distance desc, name
       
