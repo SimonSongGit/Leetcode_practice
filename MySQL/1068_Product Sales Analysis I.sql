0ed30a35ef23e7efde34b40d2d6e6c2f7e161ac5
SimonSongGit <simonsong004@gmail.com>
2020-06-01 11:48:50 -0400

    modified

--- MySQL/1068_Product Sales Analysis I.sql
+++ MySQL/1068_Product Sales Analysis I.sql
@@ -1,5 +1,10 @@
 #time: O(m+n)
-
 select product_name,year,price 
 from sales s
-     inner join product p on s.product_id=p.product_id
+     inner join product p on s.product_id=p.product_id;
+     
+     
+# use where clause which equivalent to inner join
+select product_name, year,price
+from sales s,product p
+where s.product_id=p.product_id
