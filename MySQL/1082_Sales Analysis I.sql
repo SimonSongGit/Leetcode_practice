61195f1b6b450731a396ac9de67cfc0570d4ae4c
SimonSongGit <simonsong004@gmail.com>
2020-06-03 17:50:43 -0400

    modified

--- MySQL/1082_Sales Analysis I.sql
+++ MySQL/1082_Sales Analysis I.sql
@@ -1,10 +1,19 @@
-select seller_id 
-from sales
+#duplicated rows are real repeated transactions
+SELECT seller_id 
+FROM   sales 
+GROUP  BY seller_id 
+HAVING Sum(price) = (SELECT Sum(price) 
+                     FROM   sales 
+                     GROUP  BY seller_id 
+                     ORDER  BY Sum(price) DESC 
+                     LIMIT  1); 
+
+# if duplicated rows are redundant mistakes
+select seller_id
+from (select distinct * from sales) t1
 group by seller_id
-having sum(price) = (
-                       select sum(price)
-					   from sales
-			           group by seller_id
-                       order by sum(price) desc
-                       limit 1
-					 ) 
+having sum(price) = (select sum(price)
+					 from (select distinct * from sales) t2
+                     group by seller_id
+                     order by sum(price) desc
+                     limit 1)
