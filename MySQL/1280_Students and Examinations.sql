c8aee3349cb8b500fddd3333a2ecacc80a2f15c1
SimonSongGit <simonsong004@gmail.com>
2020-06-04 17:13:50 -0400

    modified

--- MySQL/1280_Students and Examinations.sql
+++ MySQL/1280_Students and Examinations.sql
@@ -3,4 +3,16 @@ from students a
 	 cross join subjects b
      left join examinations c on a.student_id=c.student_id and b.subject_name=c.subject_name
 group by a.student_id,b.subject_name
-order by a.student_id,b.subject_name
+order by a.student_id,b.subject_name;
+
+
+
+#first get number of attended_exams before left join
+select s.student_id,student_name,s1.subject_name,ifnull(attended,0) as attended_exams
+from students s
+	 cross join subjects s1
+     left join (select *,count(*) as attended 
+                from examinations 
+                group by student_id, subject_name) e on s.student_id=e.student_id
+													and s1.subject_name=e.subject_name
+order by s.student_id,s1.subject_name
