5b14667315893f7e9cd21a9568f0af8f84ed9dce
SimonSongGit <simonsong004@gmail.com>
2020-06-05 16:49:02 -0400

    added

--- MySQL/1075_Project Employees I.sql
+++ MySQL/1075_Project Employees I.sql
@@ -1,4 +1,11 @@
 select project_id, round(avg(experience_years),2)  average_years 
 from employee e
      inner join project p on e.employee_id=p.employee_id
+group by project_id;
+
+
+# use where clause
+select project_id,round(avg(experience_years),2) as average_years 
+from project p,employee e
+where p.employee_id=e.employee_id
 group by project_id
